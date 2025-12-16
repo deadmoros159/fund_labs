9a931df09d5915c49cd0e4cc96359f417cbfeb3d
deadmoros159 <eeboy159@gmail.com>
2025-12-16 15:41:58 +0300

    Update main.c

--- pak_1/laba_3/main.c
+++ pak_1/laba_3/main.c
@@ -2,10 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <ctype.h>
+#define MAX_INT     1000000
 
 
 void is_uravnenie(float epsilon, float a, float b, float c)
 {
+
     float D = b*b - 4*a*c;
     if (D < epsilon && D < 0)
     {
@@ -26,7 +29,24 @@ void is_uravnenie(float epsilon, float a, float b, float c)
     }
     printf("Has two solutions for a = %f, b = %f, c = %f: x1 = %f , x2 = %f\n", a, b, c, x1 ,x2);
     return;
+}
 
+void all_permutations(float eps_q, float a_q, float b_q, float c_q)
+{
+    if (a_q == 0.0 && b_q == 0.00 && c_q == 0.0)
+    {
+        printf("Infinity solutions\n");
+        return;
+    }
+
+
+
+    is_uravnenie(eps_q, a_q, b_q ,c_q);
+    is_uravnenie(eps_q, a_q, c_q ,b_q);
+    is_uravnenie(eps_q, b_q, a_q ,c_q);
+    is_uravnenie(eps_q, b_q, c_q ,a_q);
+    is_uravnenie(eps_q, c_q, a_q ,b_q);
+    is_uravnenie(eps_q, c_q, b_q ,a_q);
 }
 
 int comp(const void *a, const void *b) 
@@ -66,16 +86,109 @@ int main(int argc, char *argv[])
     switch (*flag)
     {
     case 'm':
+        if (argc != 4)
+        {
+            fprintf(stderr, "Invalid amount of variables!\n");
+            return 1;
+        }
+        char* m_p_argv_1 = argv[2];
+        int m_is_argv_1_number = 1;
+        for (int i = 0; i < strlen(argv[2]); i++)
+        {
+            if (isalpha(*m_p_argv_1))
+            {
+                m_is_argv_1_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            m_p_argv_1++;
+        }
+        char* m_p_argv_2 = argv[3];
+        int m_is_argv_2_number = 1;
+        for (int i = 0; i < strlen(argv[3]); i++)
+        {
+            if (isalpha(*m_p_argv_2))
+            {
+                m_is_argv_2_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            m_p_argv_2++;
+        }
         int a = atoi(argv[2]);
         int b = atoi(argv[3]);
+        if (a > MAX_INT || b > MAX_INT)
+        {
+            fprintf(stderr, "Input lower number!\n");
+            return 1;
+        }
         if (is_kratno(a, b)) printf("%d is multiple to %d\n", a, b);
         else printf("%d is NOT multiple to %d\n", a, b);
         break;
     case 't':
+        if (argc != 6)
+        {
+            fprintf(stderr, "Invalid amount of variables!\n");
+            return 1;
+        }
+        char* t_p_argv_1 = argv[2];
+        int t_is_argv_1_number = 1;
+        for (int i = 0; i < strlen(argv[2]); i++)
+        {
+            if (isalpha(*t_p_argv_1))
+            {
+                t_is_argv_1_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            t_p_argv_1++;
+        }
+        char* t_p_argv_2 = argv[3];
+        int t_is_argv_2_number = 1;
+        for (int i = 0; i < strlen(argv[3]); i++)
+        {
+            if (isalpha(*t_p_argv_2))
+            {
+                t_is_argv_2_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            t_p_argv_2++;
+        }
+
+        char* t_p_argv_3 = argv[4];
+        int t_is_argv_3_number = 1;
+        for (int i = 0; i < strlen(argv[4]); i++)
+        {
+            if (isalpha(*t_p_argv_3))
+            {
+                t_is_argv_3_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            t_p_argv_3++;
+        }
+        char* t_p_argv_4 = argv[5];
+        int t_is_argv_4_number = 1;
+        for (int i = 0; i < strlen(argv[5]); i++)
+        {
+            if (isalpha(*t_p_argv_4))
+            {
+                t_is_argv_4_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            t_p_argv_4++;
+        }
         float eps_t = atof(argv[2]);
         float x1 = atof(argv[3]);
         float x2 = atof(argv[4]);
         float x3 = atof(argv[5]);
+        if (x1 > MAX_INT || x2 > MAX_INT || x3 > MAX_INT)
+        {
+            fprintf(stderr, "Input lower number!\n");
+            return 1;
+        }
         if (is_triangle(eps_t, x1, x2, x3))
         {
             printf("You can make triangel\n");
@@ -83,16 +196,71 @@ int main(int argc, char *argv[])
         else printf("You can NOT make triangel\n");
         break;
     case 'q':
+        if (argc != 6)
+        {
+            fprintf(stderr, "Invalid amount of variables!\n");
+            return 1;
+        }
+        char* q_p_argv_1 = argv[2];
+        int q_is_argv_1_number = 1;
+        for (int i = 0; i < strlen(argv[2]); i++)
+        {
+            if (isalpha(*q_p_argv_1))
+            {
+                q_is_argv_1_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            q_p_argv_1++;
+        }
+        char* q_p_argv_2 = argv[3];
+        int q_is_argv_2_number = 1;
+        for (int i = 0; i < strlen(argv[3]); i++)
+        {
+            if (isalpha(*q_p_argv_2))
+            {
+                q_is_argv_2_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            q_p_argv_2++;
+        }
+
+        char* q_p_argv_3 = argv[4];
+        int q_is_argv_3_number = 1;
+        for (int i = 0; i < strlen(argv[4]); i++)
+        {
+            if (isalpha(*q_p_argv_3))
+            {
+                q_is_argv_3_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            q_p_argv_3++;
+        }
+        char* q_p_argv_4 = argv[5];
+        int q_is_argv_4_number = 1;
+        for (int i = 0; i < strlen(argv[5]); i++)
+        {
+            if (isalpha(*q_p_argv_4))
+            {
+                q_is_argv_4_number = 0;
+                fprintf(stderr, "Only numbers!\n");
+                return 1;
+            }
+            q_p_argv_4++;
+        }
+        
         float eps_q = atof(argv[2]);
         float a_q = atof(argv[3]);
         float b_q = atof(argv[4]);
         float c_q = atof(argv[5]);
-        is_uravnenie(eps_q, a_q, b_q ,c_q);
-        is_uravnenie(eps_q, a_q, c_q ,b_q);
-        is_uravnenie(eps_q, b_q, a_q ,c_q);
-        is_uravnenie(eps_q, b_q, c_q ,a_q);
-        is_uravnenie(eps_q, c_q, a_q ,b_q);
-        is_uravnenie(eps_q, c_q, b_q ,a_q);
+        if (a_q > MAX_INT || b_q > MAX_INT || c_q > MAX_INT)
+        {
+            fprintf(stderr, "Input lower number!\n");
+            return 1;
+        }
+        all_permutations(eps_q, a_q, b_q ,c_q);
         break;
     default:
     printf("Invalid flag\n");
